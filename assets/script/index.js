d508d247a635026aeb4db0204277ba92b2541193
jcpurger <jc.purger@gmail.com>
2023-06-11 22:36:02 -0300

    first commit

---
+++ assets/script/index.js
@@ -0,0 +1,28 @@
+particlesJS.load('particles', 'particlesjs-config.json')
+
+let darkTheme = true;
+const particles = document.querySelector('#hero');
+const toggleThemeElement = document.querySelector('#toggleTheme');
+
+toggleTheme();
+
+toggleThemeElement.addEventListener('click', () => {
+  toggleTheme()
+})
+
+function toggleTheme() {
+  if(darkTheme) setDark()
+  else setLight()
+  
+  darkTheme = !darkTheme
+}
+
+function setDark() {
+  particles.style.backgroundColor = "#000";
+  particles.style.backgroundImage = "url('assets/images/moon.gif')";
+}
+
+function setLight() {
+  particles.style.backgroundColor = "#29b8cd";
+  particles.style.backgroundImage = "url('assets/images/sun.gif')";
+}
