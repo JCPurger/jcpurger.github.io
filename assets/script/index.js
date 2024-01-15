52e71e1bd8dcc93b8de612b17e8d30a86f4c417a
João Carlos P <19918569+JCPurger@users.noreply.github.com>
2024-01-15 20:56:18 -0300

    Feat/infos (#2)
    * info: add about
    
    * info: fix about and hero section
    
    * info: add contact and resume
    
    * info: add techs remove portifolio
    
    * change theme
    
    * remove portifolio from navbar
    
    * fix timeline colors

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,28 +1,30 @@
 particlesJS.load('particles', 'particlesjs-config.json')
 
-let darkTheme = true;
-const particles = document.querySelector('#hero');
-const toggleThemeElement = document.querySelector('#toggleTheme');
+// let darkTheme = true;
+// const particles = document.querySelector('#hero');
+// const toggleThemeElement = document.querySelector('#toggleTheme');
 
-toggleTheme();
+// toggleTheme();
 
-toggleThemeElement.addEventListener('click', () => {
-  toggleTheme()
-})
+// toggleThemeElement.addEventListener('click', () => {
+//   toggleTheme()
+// })
 
-function toggleTheme() {
-  if(darkTheme) setDark()
-  else setLight()
+// function toggleTheme() {
+//   if(darkTheme) setDark()
+//   else setLight()
   
-  darkTheme = !darkTheme
-}
+//   darkTheme = !darkTheme
+// }
 
-function setDark() {
-  particles.style.backgroundColor = "#000";
-  particles.style.backgroundImage = "url('assets/images/moon.gif')";
-}
+// function setDark() {
+//   particles.style.backgroundColor = "#000";
+//   particles.style.backgroundImage = "url('assets/images/universe-bg.webp')";
+//   particles.style.backgroundSize = "cover";
+//   particles.style.backgroundPositionX = "center"; 
+// }
 
-function setLight() {
-  particles.style.backgroundColor = "#29b8cd";
-  particles.style.backgroundImage = "url('assets/images/sun.gif')";
-}
+// function setLight() {
+//   particles.style.backgroundColor = "#29b8cd";
+//   particles.style.backgroundImage = "url('assets/images/sun.gif')";
+// }
